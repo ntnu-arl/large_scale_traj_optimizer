337d24732d6c59e65828d609f743bddff817e9c2
morten <morten.c.nissov@gmail.com>
2025-02-22 13:34:21 +0100

    functions to addStraight and addSemiCircle

--- min_snap_traj_gen/src/rounded_rectangle_node.cpp
+++ min_snap_traj_gen/src/rounded_rectangle_node.cpp
@@ -79,8 +79,9 @@ private:
 void writeFile(const Trajectory& traj, const std::string& file_name = "trajectory.csv")
 {
   // TODO avoid hard coding
-  std::string temp = "/home/morten/workspaces/noetic_ws/src/large_scale_traj_optimizer/min_snap_traj_gen/"
-                     "trajectory.csv";
+  std::string temp =
+      "/home/morten/workspaces/noetic_ws/src/large_scale_traj_optimizer/min_snap_traj_gen/"
+      "trajectory.csv";
   FILE* file = fopen(temp.c_str(), "w");
   if (file != NULL)
   {
@@ -97,6 +98,52 @@ void writeFile(const Trajectory& traj, const std::string& file_name = "trajector
               acc(0), acc(1), acc(2));
     }
   }
+
+  ROS_INFO("Done writing");
+}
+
+// TODO: add times
+void addStraight(Eigen::Vector3d& start, const Eigen::Vector3d& delta, const double velocity,
+                 std::vector<Eigen::Vector3d>& wps)
+{
+  const double total_distance = delta.norm();
+  const double dt = total_distance / velocity;
+  const size_t N = (size_t)std::round(total_distance / velocity);
+
+  Eigen::Vector3d current = start;
+  for (size_t i = 0; i < N; ++i)
+  {
+    current += delta / N;
+    wps.push_back(current);
+    // times.push_back(dt);
+  }
+
+  start += delta;
+}
+
+// TODO:: add times
+void addSemiCircle(Eigen::Vector3d& start, const Eigen::Vector3d& delta, const double omega,
+                   std::vector<Eigen::Vector3d>& wps)
+{
+  const size_t N = (size_t)std::round(M_PI / omega);
+
+  const Eigen::Vector3d finish = start + delta;
+  const Eigen::Vector3d center = (finish + start) / 2;
+  const double radius = delta.norm() / 2;
+  const double phase = std::atan2(start(1) - center(1), start(0) - center(0));
+
+  for (size_t i = 1; i <= N; ++i)
+  {
+    const double angle = omega * i + phase;
+    const double x = radius * std::cos(angle) + center(0);
+    const double y = radius * std::sin(angle) + center(1);
+    const double z = center(2);
+    // std::cout << "x: " << x << " y: " << y << '\n';
+    wps.emplace_back(x, y, z);
+    // times.push_back(1);  // REVIEW: not sure why dt is 1
+  }
+
+  start += delta;
 }
 
 int main(int argc, char** argv)
@@ -112,23 +159,67 @@ int main(int argc, char** argv)
   ros::Rate lp(10);
 
   // create trajectory
+  std::vector<Eigen::Vector3d> waypoint_vector;
+  std::vector<double> time_vector;
   // parameters
   const double radius = 1;
   const double length = 10;
-  // set start and end position
+  // set start and end position/velocity/acceleration
+  const Eigen::Vector3d start(radius, -length / 2, 0);
   Eigen::Matrix3d iS, fS;
-  iS.col(0) << radius, -length / 2, 0;
+  iS.col(0) = start;
   fS.col(0) = iS.col(0);
-  // waypoints
-  const int num_pieces = 6;
-  Eigen::Matrix<double, 3, num_pieces - 1> route;
-  route.col(0) << radius, length / 2, 0;
-  route.col(1) << 0, radius + length / 2, 0;
-  route.col(2) << -radius, length / 2, 0;
-  route.col(3) << -radius, -length / 2, 0;
-  route.col(4) << 0, -(radius + length / 2), 0;
+  waypoint_vector.push_back(start);
+  // filling remainder of trajectory
+  const double velocity = 1.0;
+  const double omega = 45.0 * M_PI / 180.0;
+  Eigen::Vector3d next = start;
+  addStraight(next, { 0, length, 0 }, velocity, waypoint_vector);
+  addSemiCircle(next, { -2 * radius, 0, 0 }, omega, waypoint_vector);
+  addStraight(next, { 0, -length, 0 }, velocity, waypoint_vector);
+  addSemiCircle(next, { 2 * radius, 0, 0 }, omega, waypoint_vector);
+
+  for (const auto& wp : waypoint_vector)
+  {
+    time_vector.push_back(0.75);
+  }
+
+  // const int num_pieces = 6;
+  // Eigen::Matrix<double, 3, num_pieces - 1> route;
+  // route.col(0) << radius, length / 2, 0;
+  // route.col(1) << 0, radius + length / 2, 0;
+  // route.col(2) << -radius, length / 2, 0;
+  // route.col(3) << -radius, -length / 2, 0;
+  // route.col(4) << 0, -(radius + length / 2), 0;
+  // Eigen::VectorXd times(num_pieces);
+  // times << 4, 2, 2, 4, 2, 2;
+
+  std::cout << "wp size: " << waypoint_vector.size() << " times size: " << time_vector.size() << '\n';
+
+  const int num_pieces = time_vector.size();
   Eigen::VectorXd times(num_pieces);
-  times << 4, 2, 2, 4, 2, 2;
+  Eigen::MatrixXd route(3, num_pieces - 1);
+  std::cout << "waypoints\n";
+  for (const auto& wp : waypoint_vector)
+  {
+    std::cout << wp.transpose() << '\n';
+  }
+  std::cout << '\n';
+  for (const auto& t : time_vector)
+  {
+    std::cout << t << '\t';
+  }
+  std::cout << '\n';
+
+  for (int i = 0; i < num_pieces; ++i)
+  {
+    times(i) = time_vector[i];
+    // because waypoint_vector includes start
+    if (i != 0)
+    {
+      route.col(i - 1) = waypoint_vector[i];
+    }
+  }
 
   std::cout << "route:\n" << route << '\n';
   std::cout << "times:\n" << times.transpose() << '\n';
@@ -143,7 +234,7 @@ int main(int argc, char** argv)
             << minJerkTraj.getPositions() << '\n';
 
   const double duration = minJerkTraj.getTotalDuration();
-  const double dt = 0.1;
+  const double dt = 0.01;
   double time = 0;
 
   // publishing
