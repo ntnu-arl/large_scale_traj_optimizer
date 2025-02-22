3d735814777582fb90cce60dcc02f42bac763959
morten <morten.c.nissov@gmail.com>
2025-02-22 11:43:12 +0100

    write to file for analysis

--- min_snap_traj_gen/src/rounded_rectangle_node.cpp
+++ min_snap_traj_gen/src/rounded_rectangle_node.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <ros/ros.h>
 #include <nav_msgs/Path.h>
@@ -29,6 +30,75 @@ void fillPoseStamped(const std::string& frame_id, const ros::Time& stamp, const 
   fillPose(vec, msg.pose);
 }
 
+class Trajectory
+{
+public:
+  Trajectory() = default;
+  ~Trajectory() = default;
+
+  void add(const double time, const Eigen::Vector3d& pos, const Eigen::Vector3d& vel, const Eigen::Vector3d& acc)
+  {
+    time_.push_back(time);
+    pos_.push_back(pos);
+    vel_.push_back(vel);
+    acc_.push_back(acc);
+  }
+
+  double getTime(const size_t i) const
+  {
+    return time_[i];
+  }
+
+  const Eigen::Vector3d& getPos(const size_t i) const
+  {
+    return pos_[i];
+  }
+
+  const Eigen::Vector3d& getVel(const size_t i) const
+  {
+    return vel_[i];
+  }
+
+  const Eigen::Vector3d& getAcc(const size_t i) const
+  {
+    return acc_[i];
+  }
+
+  size_t size() const
+  {
+    return time_.size();
+  }
+
+private:
+  std::vector<double> time_;
+  std::vector<Eigen::Vector3d> pos_;
+  std::vector<Eigen::Vector3d> vel_;
+  std::vector<Eigen::Vector3d> acc_;
+};
+
+void writeFile(const Trajectory& traj, const std::string& file_name = "trajectory.csv")
+{
+  // TODO avoid hard coding
+  std::string temp = "/home/morten/workspaces/noetic_ws/src/large_scale_traj_optimizer/min_snap_traj_gen/"
+                     "trajectory.csv";
+  FILE* file = fopen(temp.c_str(), "w");
+  if (file != NULL)
+  {
+    ROS_INFO("Writing to: %s", temp.c_str());
+    fprintf(file, "time,px,py,pz,vx,vy,vz,ax,ay,az\n");
+    for (size_t i = 0; i < traj.size(); ++i)
+    {
+      const double time = traj.getTime(i);
+      const Eigen::Vector3d pos = traj.getPos(i);
+      const Eigen::Vector3d vel = traj.getVel(i);
+      const Eigen::Vector3d acc = traj.getAcc(i);
+
+      fprintf(file, "%f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n", time, pos(0), pos(1), pos(2), vel(0), vel(1), vel(2),
+              acc(0), acc(1), acc(2));
+    }
+  }
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "min_snap_traj_node");
@@ -51,12 +121,12 @@ int main(int argc, char** argv)
   fS.col(0) = iS.col(0);
   // waypoints
   const int num_pieces = 6;
-  Eigen::Matrix<double, 3, num_pieces-1> route;
-  route.col(0) << radius, length/2, 0;
-  route.col(1) << 0, radius + length/2, 0;
-  route.col(2) << -radius, length/2, 0;
-  route.col(3) << -radius, -length/2, 0;
-  route.col(4) << 0, -(radius + length/2), 0;
+  Eigen::Matrix<double, 3, num_pieces - 1> route;
+  route.col(0) << radius, length / 2, 0;
+  route.col(1) << 0, radius + length / 2, 0;
+  route.col(2) << -radius, length / 2, 0;
+  route.col(3) << -radius, -length / 2, 0;
+  route.col(4) << 0, -(radius + length / 2), 0;
   Eigen::VectorXd times(num_pieces);
   times << 4, 2, 2, 4, 2, 2;
 
@@ -89,6 +159,7 @@ int main(int argc, char** argv)
     wp_msg.poses.push_back(ps);
   }
 
+  Trajectory traj;
   nav_msgs::Path path_msg;
   path_msg.header.frame_id = "map";
   path_msg.header.stamp = ros::Time::now();
@@ -98,12 +169,15 @@ int main(int argc, char** argv)
     const Eigen::Vector3d v = minJerkTraj.getVel(time);
     const Eigen::Vector3d a = minJerkTraj.getAcc(time);
 
+    traj.add(time, p, v, a);
     fillPose(p, ps.pose);
 
     path_msg.poses.push_back(ps);
 
     time += dt;
   }
+
+  writeFile(traj);
 
   while (ros::ok())
   {
